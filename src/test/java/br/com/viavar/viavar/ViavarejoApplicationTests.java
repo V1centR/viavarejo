6b321167f23872dc9478f75d1833aab1a169d3c8
Vicent <vicent@localhost.localdomain>
2019-11-04 23:13:27 -0500

    config project

---
+++ src/test/java/br/com/viavar/viavar/ViavarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viavar.viavar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViavarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
