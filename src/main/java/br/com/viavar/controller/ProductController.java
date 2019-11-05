d2024798a4248ee2035020ace499e6549e41d9d8
Vicent <vicent@localhost.localdomain>
2019-11-05 01:02:53 -0500

    setup request data into controller

--- src/main/java/br/com/viavar/controller/ProductController.java
+++ src/main/java/br/com/viavar/controller/ProductController.java
@@ -1,17 +1,29 @@
 package br.com.viavar.controller;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.viavar.model.Produto;
 
 @RestController
 @RequestMapping(value="/api/produto")
 public class ProductController {
 	
 	@CrossOrigin
-	@RequestMapping("/calc")
-	public @ResponseBody String findAll(){
+	@PostMapping(path="/calc",consumes = "application/json", produces = "application/json")
+	public @ResponseBody String calcProduto(@RequestBody String dataItem){
+		
+		JSONObject jsonItem = new JSONObject(dataItem);
+		
+		JSONObject produto = (JSONObject) jsonItem.get("produto");
+		JSONObject pagamento = (JSONObject) jsonItem.get("condicaoPagamento");
+		
+		
 		return "Test OK";
 	}
 }
---
+++ src/main/java/br/com/viavar/model/Pagamento.java
@@ -0,0 +1,24 @@
+package br.com.viavar.model;
+
+public class Pagamento {
+	
+	
+	public Double valorEntrada;
+	public Double qtdeParcelas;
+	
+	
+	public Double getValorEntrada() {
+		return valorEntrada;
+	}
+	public void setValorEntrada(Double valorEntrada) {
+		this.valorEntrada = valorEntrada;
+	}
+	public Double getQtdeParcelas() {
+		return qtdeParcelas;
+	}
+	public void setQtdeParcelas(Double qtdeParcelas) {
+		this.qtdeParcelas = qtdeParcelas;
+	}
+	
+
+}
